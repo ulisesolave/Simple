0c87d22cc6e97ad80f783235d2bf220d7dd36163
Ulises Olave mendoza <Ulises@macbook-air-de-ulises.local>
2018-06-06 21:38:08 -0500

    Initial Commit

---
+++ InicioSesionFB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InicioSesionFB
+//
+//  Created by Ulises Olave mendoza on 6/06/18.
+//  Copyright © 2018 Ulises Olave mendoza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
